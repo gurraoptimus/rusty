0143aaa214b57166a93f39c245b80f35b87b0d9c
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-09-19 10:37:28 +0000

    Rusty #3

---
+++ webapp/target/debug/deps/webapp-068bb691ac0748ca.d
@@ -0,0 +1,5 @@
+/workspaces/rusty/webapp/target/debug/deps/libwebapp-068bb691ac0748ca.rmeta: src/main.rs
+
+/workspaces/rusty/webapp/target/debug/deps/webapp-068bb691ac0748ca.d: src/main.rs
+
+src/main.rs:
---
+++ webapp/target/debug/deps/webapp-91ab1bb34854b818.d
@@ -0,0 +1,5 @@
+/workspaces/rusty/webapp/target/debug/deps/libwebapp-91ab1bb34854b818.rmeta: src/main.rs
+
+/workspaces/rusty/webapp/target/debug/deps/webapp-91ab1bb34854b818.d: src/main.rs
+
+src/main.rs:
