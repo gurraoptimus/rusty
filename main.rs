1627b8e97b059f8abea63b1636ac039e449fee5f
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-09-19 14:24:07 +0000

    Rusty

--- main.rs
+++ main.rs
@@ -13,20 +13,19 @@ fn app() -> Html{
     let onclick = {
         let state = state.clone();
         
-        Callback::from(move |_|{
+        Callback::from(move|_| {
             state.set(Model {
                 value: state.value + 1
             })
-        }
+        })
     }
-
+}
     html!{
     <div>
         <button {onclick}>{ "+1" } </button>
         <p { state.value }</p>
     </div>        
     }
-}
 
 fn main() {
     yew::start_app::<App>():
