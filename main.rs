784dd3467fe1f3a59d75e0b70d02669bfa8fd5f3
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-09-19 15:07:14 +0000

    Rusty

--- main.rs
+++ main.rs
@@ -5,25 +5,25 @@ struct Model{
 }
 
 #[function_component(App)]
-fn app() -> Html{
-    let state = use_state(||Model {
+fn app() -> Html {
+    let state = use_state(|| Model {
         value: 0
     });
 
     let onclick = {
         let state = state.clone();
         
-        Callback::from(move|_| {
+        Callback::from(move |_|{
             state.set(Model {
                 value: state.value + 1
             })
-        })
+        }
     }
-}
+    
     html!{
-    <div>
-        <button {onclick}>{ "+1" } </button>
-        <p { state.value }</p>
+        <div>
+         <button {onclick}>{ "+1" } </button>
+       <p { state.value }</p>
     </div>        
     }
 
