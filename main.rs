82aaf90bccba4612283793246fa4410cfbf5d0a5
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-09-19 15:47:17 +0000

    Rusty

--- main.rs
+++ main.rs
@@ -12,19 +12,21 @@ fn app() -> Html {
 
     let onclick = {
         let state = state.clone();
-        
+
         Callback::from(move |_| {
             state.set(Model {
-                 value: state.value + 1
+                value: state.value + 1
             })
-        }
-    }
+        })
+    };
 
     html! {
         <div>
-            <button {onclick}>{ "+1" }</button>
+            <button {onclick}>
+                { "+1" }
+            </button>
             <p>{ state.value }</p>
-        </div>        
+        </div>
     }
 }
 
